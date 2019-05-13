d5aae8a04882d78d151d95b1caca8ea6975269c6
jgross21 <jgrossshohola@gmail.com>
2019-05-13 08:35:00 -0500

    first

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 
-class BlackJackApp(App):
+class BlackjackApp(App):
     def build(self):
-        return BlackJackLayout()
+        return BlackjackLayout()
 
-class BlackJackLayout(BoxLayout):
+class BlackjackLayout(BoxLayout):
     pass
 
 if __name__ == '__main__':
-    app = BlackJackApp()
+    app = BlackjackApp()
     app.run()
