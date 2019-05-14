2f1e07fdb0190b52643ca7c3d85caac91dfc6f6c
jgross21 <jgrossshohola@gmail.com>
2019-05-14 14:07:50 -0500

    deck maker

--- main.py
+++ main.py
@@ -8,12 +8,58 @@ class BlackjackApp(App):
 
 class BlackjackLayout(BoxLayout):
     def create_deck(self):
-        for i in range(1, 14):
-            for x in range(4):
-                cards = x, i
-                deck = []
-                deck.append(cards)
-                print(deck)
+        deck = []
+        suits = ['H', 'C', 'D', 'S']
+        values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        amounts = [[1, 11], 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+        for suit in suits:
+            for value in values:
+                if value == 'A':
+                    amount = amounts[0]
+                    deck.append([value, suit, amount])
+                if value == '2':
+                    amount = amounts[1]
+                    deck.append([value, suit, amount])
+                if value == '3':
+                    amount = amounts[2]
+                    deck.append([value, suit, amount])
+                if value == '4':
+                    amount = amounts[3]
+                    deck.append([value, suit, amount])
+                if value == '5':
+                    amount = amounts[4]
+                    deck.append([value, suit, amount])
+                if value == '6':
+                    amount = amounts[5]
+                    deck.append([value, suit, amount])
+                if value == '7':
+                    amount = amounts[6]
+                    deck.append([value, suit, amount])
+                if value == '8':
+                    amount = amounts[7]
+                    deck.append([value, suit, amount])
+                if value == '9':
+                    amount = amounts[8]
+                    deck.append([value, suit, amount])
+                if value == '10':
+                    amount = amounts[9]
+                    deck.append([value, suit, amount])
+                if value == 'J':
+                    amount = amounts[9]
+                    deck.append([value, suit, amount])
+                if value == 'Q':
+                    amount = amounts[9]
+                    deck.append([value, suit, amount])
+                if value == 'K':
+                    amount = amounts[9]
+                    deck.append([value, suit, amount])
+
+        print(deck)
+
+
+
+
 
 
 
