4d969b0d324ccbfe3a3a7c36bb02dd0bc0c1a17d
jgross21 <jgrossshohola@gmail.com>
2019-05-20 08:33:39 -0500

    hit

--- main.py
+++ main.py
@@ -39,18 +39,18 @@ class BlackjackLayout(BoxLayout):
 
 
     def hit(self):
-        if self.user_card3.image_file == "greybackground.png":
+        if self.user_card3.card_value == 0:
             self.my_hand[2] = self.deck.pop(0)
             self.user_card3.image_file = self.my_hand[2][3]
-            self.user_card3.value = self.my_hand[2][2]
-        elif self.user_card4.image_file == "greybackground.png":
+            self.user_card3.card_value = self.my_hand[2][2]
+        elif self.user_card4.card_value == 0:
             self.my_hand[3] = self.deck.pop(0)
-            self.user_card3.image_file = self.my_hand[3][3]
-            self.user_card3.value = self.my_hand[3][2]
-        elif self.user_card5.image_file == "greybackground.png":
+            self.user_card4.image_file = self.my_hand[3][3]
+            self.user_card4.card_value = self.my_hand[3][2]
+        elif self.user_card5.card_value == 0:
             self.my_hand[4] = self.deck.pop(0)
-            self.user_card3.image_file = self.my_hand[4][3]
-            self.user_card3.value = self.my_hand[4][2]
+            self.user_card5.image_file = self.my_hand[4][3]
+            self.user_card5.card_value = self.my_hand[4][2]
 
 
 
