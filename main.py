365f8bc059d9a11b9af0052da1498e8824f1977a
jgross21 <jgrossshohola@gmail.com>
2019-05-23 13:22:48 -0500

    running deck

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ class BlackjackLayout(BoxLayout):
         super().__init__(**kwargs)
         self.deck = []
         self.my_hand = [[], [], [], [], []]
-    def create_deck(self):
         deck = []
         suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
         values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
@@ -20,7 +19,7 @@ class BlackjackLayout(BoxLayout):
 
         for suit in suits:
             for i in range(len(values)):
-                deck.append([values[i], suit, amounts[i], 'boardgamepack/PNG/Cards/card' + suit + values[i] +'.png'])
+                deck.append([values[i], suit, amounts[i], 'boardgamepack/PNG/Cards/card' + suit + values[i] + '.png'])
         self.deck = deck
 
         random.shuffle(self.deck)
@@ -46,6 +45,67 @@ class BlackjackLayout(BoxLayout):
         self.count.text = str(self.my_hand[0][2] + self.my_hand[1][2])
 
         self.name_text = 'BLACKJACK'
+    def create_deck(self):
+        self.my_hand[0] = self.deck.pop(0)
+        self.my_hand[1] = self.deck.pop(0)
+        card_1_image = self.my_hand[0][3]
+        card_1_value = self.my_hand[0][2]
+        card_2_image = self.my_hand[1][3]
+        card_2_value = self.my_hand[1][2]
+
+        self.user_card3.image_file = "greybackground.png"
+        self.user_card3.card_value = 0
+        self.user_card4.image_file = "greybackground.png"
+        self.user_card4.card_value = 0
+        self.user_card5.image_file = "greybackground.png"
+        self.user_card5.card_value = 0
+
+        self.user_card1.card_value = card_1_value
+        self.user_card1.image_file = card_1_image
+        self.user_card2.card_value = card_2_value
+        self.user_card2.image_file = card_2_image
+
+        self.count.text = str(self.my_hand[0][2] + self.my_hand[1][2])
+
+        self.name_text = 'BLACKJACK'
+        print(len(self.deck))
+        if len(self.deck) <= 10:
+            self.deck = []
+            self.my_hand = [[], [], [], [], []]
+            deck = []
+            suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
+            values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+            amounts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+            for suit in suits:
+                for i in range(len(values)):
+                    deck.append(
+                        [values[i], suit, amounts[i], 'boardgamepack/PNG/Cards/card' + suit + values[i] + '.png'])
+            self.deck = deck
+
+            random.shuffle(self.deck)
+            self.my_hand[0] = self.deck.pop(0)
+            self.my_hand[1] = self.deck.pop(0)
+            card_1_image = self.my_hand[0][3]
+            card_1_value = self.my_hand[0][2]
+            card_2_image = self.my_hand[1][3]
+            card_2_value = self.my_hand[1][2]
+
+            self.user_card3.image_file = "greybackground.png"
+            self.user_card3.card_value = 0
+            self.user_card4.image_file = "greybackground.png"
+            self.user_card4.card_value = 0
+            self.user_card5.image_file = "greybackground.png"
+            self.user_card5.card_value = 0
+
+            self.user_card1.card_value = card_1_value
+            self.user_card1.image_file = card_1_image
+            self.user_card2.card_value = card_2_value
+            self.user_card2.image_file = card_2_image
+
+            self.count.text = str(self.my_hand[0][2] + self.my_hand[1][2])
+
+            self.name_text = 'BLACKJACK'
 
 
     def hit(self):
