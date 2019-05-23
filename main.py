1690954e09c08c8c1caa24a11bec1704ac202088
jgross21 <jgrossshohola@gmail.com>
2019-05-23 12:58:36 -0500

    deal fix

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ class BlackjackLayout(BoxLayout):
 
         self.count.text = str(self.my_hand[0][2] + self.my_hand[1][2])
 
+
     def hit(self):
         if self.user_card3.card_value == 0:
             self.my_hand[2] = self.deck.pop(0)
@@ -61,7 +62,8 @@ class BlackjackLayout(BoxLayout):
             self.user_card5.image_file = self.my_hand[4][3]
             self.user_card5.card_value = self.my_hand[4][2]
             self.count.text = str(self.my_hand[0][2] + self.my_hand[1][2] + self.my_hand[2][2] + self.my_hand[3][2] + self.my_hand[4][2])
-
+        if self.count.text == 21:
+            self.name.text = 'BUST'
 
 
 
