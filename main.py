b66581baafdacb91471af80d58b84a04632a707a
jgross21 <jgrossshohola@gmail.com>
2019-05-30 13:25:20 -0500

    ace

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ class BlackjackLayout(BoxLayout):
         amounts = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
         self.dealer_count = 0
         self.user_count = 0
-        self.result = ''
+        self.user_bet = 0
+        self.bank_account = int(self.money.text[1:])
 
 
         for suit in suits:
@@ -29,46 +30,11 @@ class BlackjackLayout(BoxLayout):
         self.deck = deck
 
         random.shuffle(self.deck)
-        self.my_hand[0] = self.deck.pop(0)
-        self.my_hand[1] = self.deck.pop(0)
-        card_1_image = self.my_hand[0][3]
-        card_1_value = self.my_hand[0][2]
-        card_2_image = self.my_hand[1][3]
-        card_2_value = self.my_hand[1][2]
 
-        self.dealer_hand[0] = self.deck.pop(0)
-        self.dealer_hand[1] = self.deck.pop(0)
-        '''
-        self.dealer_hand[2] = self.deck.pop(0)
-        self.dealer_hand[3] = self.deck.pop(0)
-        self.dealer_hand[4] = self.deck.pop(0)
-        '''
-        self.dealer_count = self.dealer_hand[0][2]
-        self.dealer_number.text = str(self.dealer_count)
 
-        self.card1.image_file = self.dealer_hand[0][3]
-        self.card2.image_file = "boardgamepack/PNG/Cards/cardBack_red5.png"
-        self.card3.image_file = "greybackground.png"
-        self.card4.image_file = "greybackground.png"
-        self.card5.image_file = "greybackground.png"
-
-        self.user_card3.image_file = "greybackground.png"
-        self.user_card3.card_value = 0
-        self.user_card4.image_file = "greybackground.png"
-        self.user_card4.card_value = 0
-        self.user_card5.image_file = "greybackground.png"
-        self.user_card5.card_value = 0
-
-        self.user_card1.card_value = card_1_value
-        self.user_card1.image_file = card_1_image
-        self.user_card2.card_value = card_2_value
-        self.user_card2.image_file = card_2_image
-
-        self.user_count = self.my_hand[0][2] + self.my_hand[1][2]
-        self.count.text = str(self.user_count)
-
-        self.name_text = 'BLACKJACK'
+        self.name_text = 'BLACKJACK (DEAL TO START)'
     def create_deck(self):
+        self.user_bet = int(self.bet.text)
         self.my_hand[0] = self.deck.pop(0)
         self.my_hand[1] = self.deck.pop(0)
         card_1_image = self.my_hand[0][3]
@@ -209,6 +175,8 @@ class BlackjackLayout(BoxLayout):
             self.name_text = 'BUST'
             self.card2.image_file = self.dealer_hand[1][3]
             self.dealer_count = self.dealer_hand[0][2] + self.dealer_hand[1][2]
+            self.bank_account -= self.user_bet
+            self.money.text = '$' + str(self.bank_account)
 
 
 
@@ -300,16 +268,17 @@ class BlackjackLayout(BoxLayout):
 
         if self.dealer_count > self.user_count and self.dealer_count <=21:
             self.name_text = 'LOSE'
-            self.result = 'LOSE'
+            self.bank_account -= self.user_bet
+            self.money.text = '$' + str(self.bank_account)
 
 
         elif self.dealer_count < self.user_count or self.dealer_count > 21:
             self.name_text = 'WIN'
-            self.result = 'WIN'
+            self.bank_account += self.user_bet
+            self.money.text = '$' + str(self.bank_account)
 
         elif self.dealer_count == self.user_count:
             self.name_text = 'DRAW'
-            self.result = 'DRAW'
 
 
 
